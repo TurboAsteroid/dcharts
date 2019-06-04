20ca3524e8aa3fa0ac49f8677befcfadfca6733c
nightwelf <bpo@elem.ru>
2019-06-04 12:38:29 +0500

    simple form added

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,14 +1,47 @@
 <template>
   <v-container>
-    <i class="material-icons">10k</i> первая страница  <v-btn @click="$router.push('secondPage')">secondPage ></v-btn>
+    <i class="material-icons">10k</i> первая страница
+    <v-btn @click="$router.push('secondPage')">secondPage ></v-btn>
+
+    <v-container>
+      <v-layout>
+        <v-flex
+          xl1
+        >
+          <v-text-field
+            v-model="name"
+            lable="Имя набора данных"
+          ></v-text-field>
+          <span
+            v-for="(item, i) in userData"
+            :key="i">
+          <v-text-field
+            v-model="userData[i]"
+          ></v-text-field>
+            </span>
+          <v-btn @click="addField">Добавить</v-btn><v-btn @click="toStore">Далее</v-btn>
+          {{userData}}
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-container>
 </template>
 
 <script>
   export default {
     data: () => ({
-      a: 1
-    })
+      userData: [11, 22, 0],
+      name: 'набор данных'
+    }),
+    methods: {
+      addField () {
+        this.userData.push(0)
+      },
+      toStore () {
+        this.$store.commit('indexUserData', this.userData)
+        this.$store.commit('indexUserDataName', this.name)
+      }
+    },
   }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -5,12 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    index: {
+      userData: [],
+      name: ''
+    }
   },
   mutations: {
-
+    indexUserData (state, data) {
+      state.index.userData = data
+    },
+    indexUserDataName (state, name) {
+      state.index.name = name
+    },
   },
-  actions: {
-
+  actions: {},
+  getters: {
+    indexUserData: state => state.index.userData,
+    indexUserDataName: state => state.index.name
   }
 })
