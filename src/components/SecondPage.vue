53d0b2278982242d19380a28e514ea891d7e961e
Vasily Lobarev <vlobarev@gmail.com>
2019-06-04 10:16:45 +0500

    init

---
+++ src/components/SecondPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-btn @click="$router.back()">< back</v-btn> вторая страница
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      a: 1
+    })
+  }
+</script>
+
+<style>
+
+</style>
