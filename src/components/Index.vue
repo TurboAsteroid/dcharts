4333bac54e61a97f708b83dac28608b53cfd348b
nightwelf <bpo@elem.ru>
2019-06-04 10:16:45 +0500

    init

---
+++ src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    первая страница  <v-btn @click="$router.push('secondPage')">secondPage ></v-btn>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      a: 1
+    })
+  }
+</script>
+
+<style>
+
+</style>
