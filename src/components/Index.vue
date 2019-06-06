bf805f45c2b6abb8019ea0e3425c15f1e95d75a0
nightwelf <bpo@elem.ru>
2019-06-06 12:02:57 +0500

    arrays replaced to objects

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,50 +1,50 @@
 <template>
   <v-container>
     <v-btn @click="addArr" color="info">Добавить набор данных</v-btn>
-      <v-layout wrap>
-        <v-flex
-          :key="j+'arr'"
-          v-for="(arr, j) in userData"
-          xl2
-        >
-          <v-container>
-            <v-card>
-              <v-container>
-                <v-layout row>
-                  <v-flex xs9>
-                    <v-text-field
-                      v-model="name[j]"
-                      label="Имя набора данных"
-                    ></v-text-field>
-                  </v-flex>
-                  <v-flex xs3>
-                    <v-btn fab dark small color="red" @click="removeCol(j)">
-                      <v-icon dark>close</v-icon>
-                    </v-btn>
-                  </v-flex>
-                </v-layout>
-                <v-layout row
-                  v-for="(item, i) in userData[j]"
-                  :key="i+'item'">
-                  <v-flex xs9>
-                    <v-text-field
-                      v-model="userData[j][i]"
-                      mask="#############"
-                      label="Значение"
-                    ></v-text-field>
-                  </v-flex>
-                  <v-flex xs3>
-                    <v-btn fab outline dark small color="orange" @click="removeRow(j,i)">
-                      <v-icon dark>close</v-icon>
-                    </v-btn>
-                  </v-flex>
-                </v-layout>
-                <v-btn @click="addField(j)" color="info">Добавить значение</v-btn>
-              </v-container>
-            </v-card>
-          </v-container>
-        </v-flex>
-      </v-layout>
+    <v-layout wrap>
+      <v-flex
+        :key="j+'obj'"
+        v-for="(obj, j) in userData"
+        xl2
+      >
+        <v-container>
+          <v-card>
+            <v-container>
+              <v-layout row>
+                <v-flex xs9>
+                  <v-text-field
+                    v-model="obj.name"
+                    label="Имя набора данных"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs3>
+                  <v-btn fab dark small color="red" @click="removeCol(j)">
+                    <v-icon dark>close</v-icon>
+                  </v-btn>
+                </v-flex>
+              </v-layout>
+              <v-layout row
+                        v-for="(item, i) in obj.data"
+                        :key="i+'item'">
+                <v-flex xs9>
+                  <v-text-field
+                    v-model="obj.data[i]"
+                    mask="#############"
+                    label="Значение"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs3>
+                  <v-btn fab outline dark small color="orange" @click="removeRow(j,i)">
+                    <v-icon dark>close</v-icon>
+                  </v-btn>
+                </v-flex>
+              </v-layout>
+              <v-btn @click="addField(j)" color="info">Добавить значение</v-btn>
+            </v-container>
+          </v-card>
+        </v-container>
+      </v-flex>
+    </v-layout>
       <v-btn @click="toStore" color="success">Далее</v-btn>
   </v-container>
 </template>
@@ -52,44 +52,44 @@
 <script>
 export default {
   data: () => ({
-    objs: [{
-      userData: [11, 22, 0],
-      name: 'набор данных',
+    userData: [{
+      data: [131, 22, 0],
+      name: '3набор данных',
       val1: 0,
       val2: 0
     },
     {
-      userData: [144, 522, 30, 532],
-      name: 'набор данных 2',
+      data: [1344, 522, 30, 532],
+      name: '3набор данных 2',
       val1: 0,
       val2: 0
-    }],
-    userData: [[11, 22, 0], [144, 522, 30, 532]],
-    name: ['набор данных', 'набор данных2']
+    }]
   }),
   methods: {
     addField (j) {
-      this.userData[j].push(0)
+      this.userData[j].data.push(0)
     },
     removeRow (j, i) {
-      this.userData[j].splice(i, 1)
+      this.userData[j].data.splice(i, 1)
     },
     removeCol (j) {
       this.userData.splice(j, 1)
-      this.name.splice(j, 1)
     },
     addArr () {
-      this.userData.push([0])
-      this.name.push('новый набор данных')
+      this.userData.push({
+        data: [0],
+        name: 'набор данных',
+        val1: 0,
+        val2: 0
+      })
     },
     toStore () {
       for (let j = 0; j < this.userData.length; j++) {
-        for (let i = 0; i < this.userData[j].length; i++) {
-          this.userData[j][i] = parseInt(this.userData[j][i])
+        for (let i = 0; i < this.userData[j].data.length; i++) {
+          this.userData[j].data[i] = parseInt(this.userData[j].data[i])
         }
       }
       this.$store.commit('indexUserData', this.userData)
-      this.$store.commit('indexUserDataName', this.name)
       this.$router.push('secondPage')
     }
   }
