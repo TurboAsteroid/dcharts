ffff22c03e8f12825cc77f5a40186fe490ef84ea
Vasily Lobarev <vlobarev@gmail.com>
2019-06-17 17:12:54 +0500

    data added

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <v-btn @click="addArr" color="info">Добавить набор данных</v-btn>
+    <v-btn @click="toStore" color="success">Далее</v-btn>
     <v-layout wrap>
       <v-flex
         :key="j+'obj'"
@@ -59,56 +60,121 @@
         </v-container>
       </v-flex>
     </v-layout>
-      <v-btn @click="toStore" color="success">Далее</v-btn>
   </v-container>
 </template>
 
 <script>
-export default {
-  data: () => ({
-    userData: [{
-      data: [131, 22, 0],
-      name: 'набор данных',
-      val1: 50,
+export default { data: () => ({
+  userData: [
+    {
+      data: [95, 97, 99, 100],
+      name: 'СИЗ: Промышленная безопасность',
+      val1: 98,
       val2: 100
     },
     {
-      data: [1344, 522, 30, 532],
-      name: 'набор данных 2',
+      data: [95, 99, 100, 98],
+      name: 'ЛПП: Промышленная безопасность',
+      val1: 98,
+      val2: 100
+    },
+    {
+      data: [99, 93, 100, 98],
+      name: 'Численность: Кадровая комплектация',
+      val1: 95,
+      val2: 100
+    },
+    {
+      data: [5, 2, 17, 10, 2, 7],
+      name: 'Текучесть: Кадровая комплектация',
+      val1: 3,
+      val2: 12
+    },
+    {
+      data: [99, 100, 101, 98, 100, 99],
+      name: 'Трудовая дисциплина: Кадровая комплектация',
       val1: 100,
-      val2: 600
+      val2: 99
+    },
+    {
+      data: [3054, 2602, 4125],
+      name: 'Количество обученных сотрудников',
+      val1: 2500,
+      val2: 2600
+    },
+    {
+      data: [100, 99, 98, 102, 105, 95],
+      name: 'Фонд оплаты труда',
+      val1: 100,
+      val2: 99
+    },
+    {
+      data: [43528, 44564, 42888, 45821, 41433],
+      name: 'Средняя заработная плата',
+      val1: 42000,
+      val2: 45000
+    },
+    {
+      data: [97, 99, 105, 99, 99, 103],
+      name: 'Ежедневный учет: Производство',
+      val1: 98,
+      val2: 100
+    },
+    {
+      data: [96, 106, 101, 100, 100, 102],
+      name: 'Ежемесячный учет: Производство',
+      val1: 98,
+      val2: 100
+    },
+    {
+      data: [100, 100, 105, 101, 102, 103],
+      name: 'Накопительный учет: Производство',
+      val1: 98,
+      val2: 100
+    },
+    {
+      data: [100, 99, 98, 102, 100, 99],
+      name: 'Накопительный итог: Данные СБ',
+      val1: 100,
+      val2: 102
+    },
+    {
+      data: [150, 135],
+      name: 'Социальная напряженность',
+      val1: 80,
+      val2: 140
     }]
-  }),
-  methods: {
-    addField (j) {
-      this.userData[j].data.push(0)
-    },
-    removeRow (j, i) {
-      this.userData[j].data.splice(i, 1)
-    },
-    removeCol (j) {
-      this.userData.splice(j, 1)
-    },
-    addArr () {
-      this.userData.push({
-        data: [0],
-        name: 'набор данных',
-        val1: 0,
-        val2: 0
-      })
-    },
-    toStore () {
-      for (let j = 0; j < this.userData.length; j++) {
-        for (let i = 0; i < this.userData[j].data.length; i++) {
-          this.userData[j].data[i] = parseInt(this.userData[j].data[i])
-        }
-        this.userData[j].val1 = parseInt(this.userData[j].val1)
-        this.userData[j].val2 = parseInt(this.userData[j].val2)
+}),
+methods: {
+  addField (j) {
+    this.userData[j].data.push(0)
+  },
+  removeRow (j, i) {
+    this.userData[j].data.splice(i, 1)
+  },
+  removeCol (j) {
+    this.userData.splice(j, 1)
+  },
+  addArr () {
+    this.userData.push({
+      data: [0],
+      name: 'набор данных',
+      val1: 0,
+      val2: 0
+    })
+  },
+  toStore () {
+    for (let j = 0; j < this.userData.length; j++) {
+      for (let i = 0; i < this.userData[j].data.length; i++) {
+        this.userData[j].data[i] = parseInt(this.userData[j].data[i])
       }
-      this.$store.commit('indexUserData', this.userData)
-      this.$router.push('secondPage')
+      this.userData[j].val1 = parseInt(this.userData[j].val1)
+      this.userData[j].val2 = parseInt(this.userData[j].val2)
     }
+    this.$store.commit('indexUserData', this.userData)
+    this.$router.push('secondPage')
   }
+}
 }
 </script>
 
