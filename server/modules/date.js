08d346521b3a91d06dd832112d3056a397e0c8e8
naa <novoselov_aa@elem.ru>
2019-07-12 12:43:40 +0500

    fetch запрос по параметру и дате

---
+++ server/modules/date.js
@@ -0,0 +1,14 @@
+const createDate = () => {
+    const count = 12;
+    let dateCurrent = new Date()
+    let date;
+    let result = '';
+
+    for(let i = 2; i < count + 2; i++) {
+        date = new Date(dateCurrent.getFullYear(), dateCurrent.getMonth() - i, dateCurrent.getDay())
+        result += `${date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : 
+                    date.getMonth() + 1}.${date.getFullYear()},`                  
+    }
+    return result;
+}
+module.exports = createDate;
