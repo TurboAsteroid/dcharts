2a668476716237c63a1d3d14f7dbd3c04a903e15
Vasily Lobarev <vlobarev@gmail.com>
2019-07-12 12:43:40 +0500

    fetch запрос по параметру и дате

--- server/index.js
+++ server/index.js
@@ -3,10 +3,18 @@ const { buildSchema } = require('graphql');
 const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
 const fsPromise = require('fs-readfile-promise');
+const fetch = require('node-fetch');
+const https = require("https");
+const agent = new https.Agent({
+    rejectUnauthorized: false
+})
+
+const createDate = require('./modules/date.js');
 
 const schema = buildSchema(`
     type Query {
         getLibrary: [Library]
+        getDataByParametr(parametr: String!): [DataParametrs]
     }
     type Library {
         id: ID!
@@ -16,11 +24,31 @@ const schema = buildSchema(`
         val2: Int
         link: String
     }
+    type DataParametrs {
+        
+        data: [String]!
+        label: [String]!
+    }
 `)
 const rootValue = {
     getLibrary:async() => {
-        let library = JSON.parse(await fsPromise('./server/library.json', 'utf8'));
-        return library
+        try {
+            let library = JSON.parse(await fsPromise('./server/library.json', 'utf8'));
+            return library
+        } catch (e) {
+            console.log(e.message)
+        }
+    },
+    getDataByParametr: async ({parametr}) => {
+        try {
+            console.log(parametr)
+            let response = await fetch(`https://elem-pre.elem.ru/spline/api/salary?filter=${parametr}&date=${createDate()}`, {agent})
+            let data = await response.json();
+            console.log(data);
+            // return data
+        } catch (e) {
+            console.log(e.message)
+        }
     }
 }
 
