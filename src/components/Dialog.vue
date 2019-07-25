9d9b1a5fc1b864ebba97a659f17d1884cb4cb463
Vasily Lobarev <vlobarev@gmail.com>
2019-07-25 09:33:20 +0500

    изменена стр. Index

---
+++ src/components/Dialog.vue
@@ -0,0 +1,107 @@
+<template>
+    <v-dialog
+        v-model="$store.state.dialog"
+        max-width="600"
+      >
+        <v-card>
+          <v-card-title>
+            <v-layout row>
+              <v-flex xs10>
+                <span class="headline">Настройка набора данных</span>
+              </v-flex>
+              <v-flex xs2>
+                <v-btn fab dark small color="red" @click="$store.commit('changeDialog',{bool: false})">
+                  <v-icon dark>close</v-icon>
+              </v-btn>
+            </v-flex>
+            </v-layout>
+          </v-card-title>
+
+          <v-card-text>
+                <v-layout row>
+                    <v-flex xs9>
+                    <v-text-field
+                        v-model="note.name"
+                        label="Имя набора данных"
+                    ></v-text-field>
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs12>
+                    <v-text-field
+                        v-model="note.link"
+                        label="link"
+                    ></v-text-field>
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs6>
+                    <v-text-field
+                        v-model="note.val1.value"
+                        label="Первый порог"
+                    ></v-text-field>
+                    </v-flex>
+                    <v-flex xs6>
+                    <v-text-field
+                    v-model="note.val2.value"
+                    label="Второй порог"
+                    ></v-text-field>
+                </v-flex>
+                </v-layout>
+                <v-layout row
+                    v-for="(item, i) in note.data"
+                    :key="i+'item'">
+                    <v-flex xs9>
+                    <v-text-field
+                        v-model="note.data[i]"
+                        mask="#############"
+                        label="Значение"
+                    ></v-text-field>
+                    </v-flex>
+                    <v-flex xs3>
+                    <v-btn fab outline dark small color="orange" @click="removeRow(i)">
+                        <v-icon dark>close</v-icon>
+                    </v-btn>
+                    </v-flex>
+                </v-layout>
+                <v-btn @click="addField()" color="info">Добавить значение</v-btn>
+          </v-card-text>
+
+          <!-- <v-card-actions>
+            <v-btn
+              color="error"
+              text
+              @click="$store.commit('changeDialog',{bool: false})"
+            >
+              Отменить
+            </v-btn>
+              <v-spacer></v-spacer>
+            <v-btn
+              color="success"
+              text
+              @click="$store.commit('changeDialog',{bool: false})"
+            >
+              Применить
+            </v-btn>
+          </v-card-actions> -->
+        </v-card>
+      </v-dialog>
+</template>
+
+
+<script>
+export default {
+    computed:{
+        note() {
+          return this.$store.state.currentNote
+        }
+    }, methods: {
+      addField () {
+        this.note.data.push(0)
+      },
+      removeRow (i) {
+        this.note.data.splice(i, 1)    
+      },
+    }
+}
+</script>
